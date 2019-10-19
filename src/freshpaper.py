0ba639579f598d201c45238cf8951f9b695b72da
pratiksaxena <pratik2709@gmail.com>
2019-10-19 15:13:05 +0530

    added system utils

--- src/freshpaper.py
+++ src/freshpaper.py
@@ -1,13 +1,12 @@
 import logging
-import os
 import sys
-from random import choice
 
 import click
 from pip._vendor.requests import ConnectionError
 
 from Constants import BING_IMAGE_DESCRIPTION, NASA_IMAGE_DESCRIPTION, IMAGE_SOURCES
 from src.DownloadUtils import DownloadUtils
+from src.SystemUtils import SystemUtils
 from src.WallpaperUtils import WallpaperUtils
 
 try:
@@ -26,65 +25,29 @@ logging.basicConfig(level=logging.INFO, format="%(message)s")
 log = logging.getLogger(__name__)
 
 
-def get_saved_wallpaper(wall_dir):
-    """ returns random saved wallpaper's path """
-
-    files = os.listdir(wall_dir)
-    if files:
-        log.info("\nRandomly picking from saved wallpapers.")
-        image_name = choice(files)
-        image_path = os.path.join(os.sep, wall_dir, image_name)
-        return image_path
-    else:
-        log.error("There is no wallpaper available offline.")
-        sys.exit(1)
-
-
-def get_wallpaper_directory():
-    """ check if `default` wallpaper download directory exists or not, create if doesn't exist """
-    pictures_dir = ""
-    wall_dir_name = "freshpaper"
-    os.path.join(os.sep, os.path.expanduser("~"), "a", "freshpaper")
-    if sys.platform.startswith("win32"):
-        pictures_dir = "My Pictures"
-    elif sys.platform.startswith("darwin"):
-        pictures_dir = "Pictures"
-    elif sys.platform.startswith("linux"):
-        pictures_dir = "Pictures"
-    wall_dir = os.path.join(
-        os.sep, os.path.expanduser("~"), pictures_dir, wall_dir_name
-    )
-
-    if not os.path.isdir(wall_dir):
-        log.error("wallpaper directory does not exist.")
-        os.makedirs(wall_dir)
-        log.info("created wallpaper directory at: {}".format(wall_dir))
-
-    return wall_dir
-
-
 @click.group(invoke_without_command=True)
 @click.pass_context
 @click.option(
     "--source",
-    default="bing",
+    default="nasa",
     type=click.Choice(IMAGE_SOURCES),
     help="Source for setting the wallpaper.",
 )
 def main(context, source):
     if context.invoked_subcommand is None:
-        directory_name = get_wallpaper_directory()  # Wallpaper directory name
         wallpaper = WallpaperUtils()
         downloadUtils = DownloadUtils()
+        systemUtils = SystemUtils()
+        directory_name = systemUtils.get_wallpaper_directory()  # Wallpaper directory name
         freshpaperSources = construct_freshpaper_sources(downloadUtils.download_image_bing,
-                                     downloadUtils.download_image_nasa)
+                                                         downloadUtils.download_image_nasa)
         try:
             download_image = freshpaperSources.get(source)["download"]
             image_path = download_image(directory_name)
 
             wallpaper.set_wallpaper(image_path)
         except ConnectionError:
-            image_path = get_saved_wallpaper(directory_name)
+            image_path = systemUtils.get_saved_wallpaper(directory_name)
             wallpaper.set_wallpaper(image_path)
         except Exception as e:
             log.error(e)
