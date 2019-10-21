764e74575ed3851ad0332c8aec013b05f6318801
Ashish285 <ashu28595@gmail.com>
2019-10-21 11:30:07 +0530

    Adds unsplash random and daily source

--- freshpaper.py
+++ freshpaper.py
@@ -215,9 +215,85 @@ def download_image_nasa(download_dir, image_extension="jpg"):
         log.error("Something went wrong..\nMaybe Internet is not working...")
         raise ConnectionError
 
+def download_image_unsplash_daily(download_dir, image_extension="jpg"):
+    """
+    Download & save the image
+    :param download_dir: directory where to download the image
+    :param image_extension: directory where to download the image
+    :return: downloaded image path
+    """
+    # mkt(s) HIN, EN-IN
+
+    url = "https://source.unsplash.com/daily"
+
+    try:
+        image_url = url
+
+        image_name = 'unsplash_daily'
+
+        date_time = datetime.now().strftime("%d_%m_%Y")
+        image_file_name = "{image_name}_{date_stamp}.{extention}".format(
+            image_name=image_name, date_stamp=date_time, extention=image_extension
+        )
+
+        image_path = os.path.join(os.sep, download_dir, image_file_name)
+        log.debug("download_dir: {}".format(download_dir))
+        log.debug("image_file_name: {}".format(image_file_name))
+        log.debug("image_path: {}".format(image_path))
+
+        if os.path.isfile(image_path):
+            log.info("No new wallpaper yet..updating to latest one.\n")
+            return image_path
+
+        log.info("Downloading...")
+        urlretrieve(image_url, filename=image_path)
+        return image_path
+    except URLError:
+        log.error("Something went wrong..\nMaybe Internet is not working...")
+        raise ConnectionError
+
+def download_image_unsplash_random(download_dir, image_extension="jpg"):
+    """
+    Download & save the image
+    :param download_dir: directory where to download the image
+    :param image_extension: directory where to download the image
+    :return: downloaded image path
+    """
+    # mkt(s) HIN, EN-IN
+
+    url = "https://source.unsplash.com/random/1920x1080"
+
+    try:
+        image_url = url
+
+        image_name = 'unsplash_random'
+
+        date_time = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
+        image_file_name = "{image_name}_{date_stamp}.{extention}".format(
+            image_name=image_name, date_stamp=date_time, extention=image_extension
+        )
+
+        image_path = os.path.join(os.sep, download_dir, image_file_name)
+        log.debug("download_dir: {}".format(download_dir))
+        log.debug("image_file_name: {}".format(image_file_name))
+        log.debug("image_path: {}".format(image_path))
+
+        if os.path.isfile(image_path):
+            log.info("No new wallpaper yet..updating to latest one.\n")
+            return image_path
+
+        log.info("Downloading...")
+        urlretrieve(image_url, filename=image_path)
+        return image_path
+    except URLError:
+        log.error("Something went wrong..\nMaybe Internet is not working...")
+        raise ConnectionError
+
 freshpaperSources = {
     "bing": {"download": download_image_bing, "description": "Bing photo of the day"},
     "nasa": {"download": download_image_nasa, "description": "NASA photo of the day"},
+    "unsplash_daily": {"download": download_image_unsplash_daily, "description": "Unsplash photo of the day"},
+    "unsplash_random": {"download": download_image_unsplash_random, "description": "Unsplash random photo"},
 }
 
 
