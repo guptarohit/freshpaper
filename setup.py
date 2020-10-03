0279073e028afbdefdc442ee2374b20bc5430d9c
aadilraza339 <aadil18@navgurukul.org>
2020-10-03 17:11:06 +0530

    Update setup.py
    Updates for 3.8.4

--- setup.py
+++ setup.py
@@ -76,6 +76,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.8.4",
         "Programming Language :: Python :: 3.9",
         "Operating System :: MacOS",
         "Operating System :: POSIX :: Linux",
