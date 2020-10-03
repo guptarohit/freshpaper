f8cbb1dae283c434b7ff1ea4edbfcbd2bf11aca4
Harshita <55912888+harshitacodes@users.noreply.github.com>
2020-10-03 15:13:26 +0530

    Update setup.py
    remove python 2 versions support

--- setup.py
+++ setup.py
@@ -72,8 +72,6 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Topic :: Utilities",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
